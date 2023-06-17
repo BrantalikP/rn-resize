c969224f711fda3c4787c1ddb82e1ed4859126c0
Petr Brantalík <grounipteam@gmail.com>
2023-06-17 08:04:43 +0200

    fix: ts fix

--- src/hooks/useStyles/index.ts
+++ src/hooks/useStyles/index.ts
@@ -6,12 +6,10 @@ import { useTheme } from '../../contexts/Theme';
 import { CustomStyles } from '../../types';
 
 export const useStyles = <T extends CustomStyles, U>(
-  makeStyles: ((theme: U | null) => T) | T
+  makeStyles: (theme: U | null) => T
 ): StyleSheet.NamedStyles<T> => {
   const { theme } = useTheme<U>();
-  const styles = createStyle(
-    typeof makeStyles === 'function' ? makeStyles(theme) : makeStyles
-  );
+  const styles = createStyle(makeStyles(theme));
 
   return styles;
 };
